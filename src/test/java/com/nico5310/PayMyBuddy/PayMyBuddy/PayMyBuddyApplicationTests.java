610c5e34d02bf7d32e3ebddb01a3e3b7a3d3b933
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-05-29 18:50:22 +0200

    Origin commit

---
+++ src/test/java/com/nico5310/PayMyBuddy/PayMyBuddy/PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico5310.PayMyBuddy.PayMyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
