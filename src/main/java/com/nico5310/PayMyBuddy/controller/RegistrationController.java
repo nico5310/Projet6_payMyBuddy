4c0f948ad7140187d60370716523e1ea28e8a789
Nicolas BIANCUCCI <nico5310@gmail.com>
2021-06-22 14:41:56 +0200

    Fix PayMyBuddy Application

--- src/main/java/com/nico5310/PayMyBuddy/controller/RegistrationController.java
+++ src/main/java/com/nico5310/PayMyBuddy/controller/RegistrationController.java
@@ -8,46 +8,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
 
-/**
- * Registration view controller
- * @author Nicolas
- */
+
 @Controller
 public class Registration {
 
-    /**
-     * @see UserService
-     */
     @Autowired
     private UserService userService;
 
-    /**
-     * Get sign up view
-     * @param model the model
-     * @return the registration view
-     */
-    @PreAuthorize("isAnonymous()")
+
     @GetMapping(value = "/registration")
     public String addUser(Model model) {
-
         User user = new User();
         model.addAttribute(user);
         return "registration";
-
     }
 
-    /**
-     * Get add user view
-     *
-     * @return the add user from view
-     */
-    @PreAuthorize("isAnonymous()")
-    @GetMapping(value = "/save")
-    public String addUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+    @PostMapping(value = "/save")
+    public String addUser(@Valid @ModelAttribute("user") User user, Model model) {
 
         userService.saveUser(user);
         return "redirect:/login";
---
+++ src/main/java/com/nico5310/PayMyBuddy/exception/NoCreateUserPossibleException.java
@@ -0,0 +1,2 @@
+package com.nico5310.PayMyBuddy.exception;public class NoSavePossibile {
+}
